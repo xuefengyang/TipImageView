e2156be95bcb33a8bcc7f2e3da60b7f04713cc7a
xuefengyang <yangxuefengy@gmail.com>
2015-11-07 16:51:08 +0800

    init commit

---
+++ app/src/main/java/com/xuefengyang/tipimage/demo/TipDemoActivity.java
@@ -0,0 +1,16 @@
+package com.xuefengyang.tipimage.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xuefengyang.tipimageview.TipImageView;
+
+public class TipDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_demo);
+
+    }
+}
